25b761a4ef2c6b6fc936846cd315f7bbb4cfbc81
bordun <jean-emmanuel.doucet@groolot.net>
2020-01-26 16:54:32 +0100

    osc: utf8 read/write is now supported upstream

--- src/server/osc/osc.js
+++ src/server/osc/osc.js
@@ -5,17 +5,4 @@ var osc = require('osc/src/osc.js'),
 osc.UDPPort = transports.UDPPort
 osc.TCPSocketPort = transports.TCPSocketPort
 
-osc.writeUtfString = (str)=>{
-    return osc.writeString(utf8.encode(str))
-}
-
-osc.readUtfString = (dv, offsetState)=>{
-    return osc.readString(dv, offsetState)
-}
-
-osc.argumentTypes.s = osc.argumentTypes.S = {
-    reader: 'readUtfString',
-    writer: 'writeUtfString'
-}
-
 module.exports = osc
