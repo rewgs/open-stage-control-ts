96c59ee3bd79bcd65f922dcb1f23ab0bdddf4397
bordun <jean-emmanuel.doucet@groolot.net>
2020-03-23 14:42:12 +0100

    missing file

---
+++ src/client/widgets/basics/menu-base.js
@@ -0,0 +1,48 @@
+var Widget = require('../common/widget'),
+    deepEqual = require('../../utils')
+
+class MenuBase extends Widget {
+
+    constructor(options) {
+
+        super(options)
+
+        this.value = undefined
+        this.values = []
+        this.keys = []
+        this.objectInValues = false
+
+    }
+
+    parseValues() {
+
+        this.values = []
+        this.keys = []
+
+        var values = this.getProp('values') || []
+
+        if (!Array.isArray(values) && !(typeof values === 'object' && values !== null)) {
+            values = values !== '' ? [values] : []
+        }
+
+        this.values = !Array.isArray(values) ? Object.values(values) : values
+        this.keys = !Array.isArray(values) ? Object.keys(values) : this.values
+        this.objectInValues = this.values.some(x=>typeof x === 'object')
+
+    }
+
+    getIndex(value) {
+
+        var index = this.values.indexOf(value)
+        if (index === -1 && this.objectInValues) {
+            for (var i = 0; i < this.values.length; i++) {
+                if (deepEqual(value, this.values[i])) return i
+            }
+        }
+        return i
+
+    }
+
+}
+
+module.exports = MenuBase
