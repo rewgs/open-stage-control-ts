7eaa6c89a0ace3d78cbfc1d6998a88950fff047b
bordun <jean-emmanuel.doucet@groolot.net>
2020-04-04 12:49:20 +0200

    package: copy fonts, nosymlink

--- scripts/copy-assets.js
+++ scripts/copy-assets.js
@@ -5,10 +5,8 @@ var cpr = require('cpr'),
         ['../resources/images/logo-nobadge.png', '../app/assets/favicon.png'],
         ['../resources/images/logo.png', '../app/assets/logo.png'],
         ['../LICENSE', '../app/LICENSE'],
-        ['../src/python/head.py', '../app/server/python/head.py'],
-        ['../src/python/list.py', '../app/server/python/list.py'],
-        ['../src/python/utils.py', '../app/server/python/utils.py'],
-        ['../src/python/midi.py', '../app/server/python/midi.py'],
+        ['../src/python/', '../app/server/python/'],
+        ['../resources/fonts/', '../app/assets/fonts/'],
     ]
 
 for (var i in files) {
