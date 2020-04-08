8e67a5bb051f48289759340e687fdce8612f4bed
bordun <jean-emmanuel.doucet@groolot.net>
2020-04-08 16:16:49 +0200

    docs: update discourse logo && script

---
+++ resources/docs/extra/scripts.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    if (window.location.hash) {
+        // show target in tabs/details
+        document.querySelectorAll('[id='+window.location.hash.split("#")[1]+']').forEach(function(item){
+            var tab = item.closest('.tabbed-content'),
+                details = item.closest('details')
+            if (tab) tab.previousSibling.click()
+            if (item) {
+                item.scrollIntoView()
+                document.documentElement.scrollTop -= 70
+            }
+        })
+    }
+
+
+})
