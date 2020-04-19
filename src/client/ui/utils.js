8d5616a51365a555d9d12623cd2029743322fdaa
bordun <jean-emmanuel.doucet@groolot.net>
2020-04-19 20:39:15 +0200

    lint: misc fixes

--- src/client/ui/utils.js
+++ src/client/ui/utils.js
@@ -3,7 +3,7 @@ module.exports = {
 
 
     icon: function(i) {
-	var iclass = i.split('.').join(' fa-')
+        var iclass = i.split('.').join(' fa-')
         return `<i class="fa fa-fw fa-${iclass}"></i>`
     },
     iconify:function(string){
