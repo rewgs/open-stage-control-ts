54c0b7fb29181b79ce4897c82e719936b4f56859
bordun <jean-emmanuel.doucet@groolot.net>
2020-05-05 15:01:49 +0200

    https: fix

--- src/server/settings.js
+++ src/server/settings.js
@@ -156,5 +156,5 @@ module.exports = {
     cli: cli,
     configPath: baseDir,
     infos: infos,
-    appAddresses: ()=>address(settings.options.ssl ? 'https://' : 'http://', settings.options.port || 8080)
+    appAddresses: ()=>address(settings.options['use-ssl'] ? 'https://' : 'http://', settings.options.port || 8080)
 }
