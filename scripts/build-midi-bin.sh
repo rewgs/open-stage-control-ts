c1f9c21678674d11ff615c6dc76708e6c494e881
bordun <jean-emmanuel.doucet@groolot.net>
2020-07-27 18:30:07 +0200

    midi: add version

--- scripts/build-midi-bin.sh
+++ scripts/build-midi-bin.sh
@@ -7,15 +7,15 @@ cd src/python
 pyinstaller midi.py --onefile
 
 docker run -v "$(pwd):/src/" cdrx/pyinstaller-linux:python3
-mv dist/linux dist/linux-x64
+mv dist/linux/midi midi-linux-x64
 
 docker run -v "$(pwd):/src/" cdrx/pyinstaller-linux:python3-32bit
-mv dist/linux dist/linux-ia32
+mv dist/linux/midi midi-linux-ia32
 
 docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows:python3
-mv dist/windows dist/windows-x64
+mv dist/windows/midi.exe midi-windows-x64.exe
 
 docker run -v "$(pwd):/src/" cdrx/pyinstaller-windows:python3-32bit
-mv dist/windows dist/windows-ia32
+mv dist/windows/midi.exe midi-windows-ia32.exe
 
-rm -rf __pycache__ build midi.spec
+rm -rf __pycache__ build midi.spec dist
