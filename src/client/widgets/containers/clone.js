7733ae807f85120258f48e329f851a688efcea96
bordun <jean-emmanuel.doucet@groolot.net>
2020-09-06 17:08:38 +0200

    clone: rm colorBg & strokeWidth (should be overridden via "props")

--- src/client/widgets/containers/clone.js
+++ src/client/widgets/containers/clone.js
@@ -26,7 +26,7 @@ class Clone extends Container() {
             props: {type: 'object', value: {}, help: 'Cloned widget\'s properties to override'},
 
         }, [
-            'colorText', 'colorWidget', 'colorFill', 'colorStroke', 'alphaStroke', 'alphaFillOff', 'alphaFillOn', 'padding',
+            'colorBg', 'colorText', 'colorWidget', 'colorFill', 'colorStroke', 'alphaStroke', 'alphaFillOff', 'alphaFillOn', 'padding', 'strokeWidth',
             'label', 'color',
             '_value', 'default', 'value', 'script', 'linkId',
             '_osc', 'decimals', 'address', 'preArgs', 'typeTags', 'target', 'ignoreDefaults', 'bypass'
