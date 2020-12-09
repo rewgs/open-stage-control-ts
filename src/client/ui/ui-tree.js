97c31aca17c5ae26d49c11271f920c36a590b31a
bordun <jean-emmanuel.doucet@groolot.net>
2020-12-09 16:29:52 +0100

    ui: tree: defer tree update when not mounted

--- src/client/ui/ui-tree.js
+++ src/client/ui/ui-tree.js
@@ -21,7 +21,8 @@ class UiTree extends UiWidget {
         this.mounted = false
         this.list = this.container.appendChild(html`<ol style="--depth: 1"></ol`)
         this.dragDummy = html`<span></span>`
-
+        this.deferredUpdateTimeout = null
+        this.editor = options.editor
 
         this.expanded = {}
 
@@ -84,6 +85,21 @@ class UiTree extends UiWidget {
     }
 
     updateTree(selectedWidgets) {
+
+        if (this.parent.mounted) {
+            this.deferredUpdateTree(selectedWidgets)
+        } else {
+            clearTimeout(this.deferredUpdateTimeout)
+            this.deferredUpdateTimeout = setTimeout(()=>{
+                this.deferredUpdateTree(selectedWidgets)
+            })
+        }
+
+    }
+
+    deferredUpdateTree(selectedWidgets) {
+
+        this.deferredUpdateTimeout = null
 
         for (var s of this.sortables) {
             s.destroy()
