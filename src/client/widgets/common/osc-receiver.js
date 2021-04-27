rewgs/open-stage-control-ts

commit 7e0921fb894d97990262290cdf508b095bc78c2a

osc listener: only json-parse stringified objects/arrays/bool/null, not stringified primitives

src/client/widgets/common/osc-receiver.js
@@ -1,5 +1,5 @@
 var osc = require('../../osc'),
-    {isJSON} = require('../../utils')
+    {isJSONObject} = require('../../utils')
 
 module.exports = class OscReceiver {
 
@@ -7,7 +7,7 @@ module.exports = class OscReceiver {
 
         var {address, value, parent, propName, usePreArgs} = options
 
-        if (typeof value === 'string' && isJSON(value)) {
+        if (typeof value === 'string' && isJSONObject(value)) {
             try {
                 this.value = JSON.parseFlex(value)
             } catch (err) {
@@ -67,7 +67,7 @@ module.exports = class OscReceiver {
             }
             var value = args.slice(preArgs.length)
             if (value.length < 2) value = value[0]
-            if (typeof value === 'string' && isJSON(value)) {
+            if (typeof value === 'string' && isJSONObject(value)) {
                 try {
                     this.value = JSON.parseFlex(value)
                 } catch (err) {

src/client/utils.js
@@ -43,6 +43,11 @@ module.exports = {
         // ref in source: https://github.com/douglascrockford/JSON-js
         return ' 	\n+-eE{([0123456789tfn"'.indexOf(str[0]) !== -1
 
+    },
+
+    isJSONObject: function(str) {
+        // same, but only for object/array/bool/null
+        return '{[tfn'.indexOf(str[0]) !== -1
     }
 
 }

commit 591a996fd6e07f4f2bb5decee683119122114c9e

osc listener: default value should always be parsed as json (regression in 7e0921fb894d97990262290cdf508b095bc78c2a)

src/client/widgets/common/osc-receiver.js
@@ -1,5 +1,5 @@
 var osc = require('../../osc'),
-    {isJSONObject} = require('../../utils')
+    {isJSON, isJSONObject} = require('../../utils')
 
 module.exports = class OscReceiver {
 
@@ -7,7 +7,7 @@ module.exports = class OscReceiver {
 
         var {address, value, parent, propName, usePreArgs} = options
 
-        if (typeof value === 'string' && isJSONObject(value)) {
+        if (typeof value === 'string' && isJSON(value)) {
             try {
                 this.value = JSON.parseFlex(value)
             } catch (err) {