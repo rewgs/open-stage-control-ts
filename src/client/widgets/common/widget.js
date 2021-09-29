e8004e2db5340d2c241838d7bde9e42a4f4a45c0
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2021-09-29 09:41:05 +0200

    doc: better explaination of css vars

--- src/client/widgets/common/widget.js
+++ src/client/widgets/common/widget.js
@@ -93,7 +93,7 @@ class Widget extends EventEmitter {
                 ]},
                 css: {type: 'string', value: '', help: [
                     'CSS rules. See <a href="https://openstagecontrol.ammd.net/docs/customization/css-tips/">documentation</a>.',
-                    'Available css variables:',
+                    'Some style-related properties can be set or read from css using css variables:',
                     ...Class.cssVariables.map(x=>'- `' + x.css + '`: `' + x.js + '`')
                 ]},
             },
