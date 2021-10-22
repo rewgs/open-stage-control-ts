897db8108dcdbefe26f0fa6bfb03619c8e20c228
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2021-10-22 12:39:48 +0200

    misc: print full debug stack in browser debug console in case widget.errorProp() is not precise enough

--- src/client/widgets/common/widget.js
+++ src/client/widgets/common/widget.js
@@ -974,6 +974,7 @@ class Widget extends EventEmitter {
         if (widget.parent !== widgetManager && widget.parent.getProp('type') === 'clone') widget = widget.parent
 
         console._error(`${id}.${name} ${type} error${line}: ${error}`)
+        console.debug(error)
         uiConsole.log('error', `<span class="edit-widget" data-widget="${widget.builtIn ? widget.parent.hash : widget.hash}">${id}</span>.${name} ${type} error${line}: ${error}`, true)
     }
 
