37ecc41826d5bfebcb1519164d2fded3e43e4b9d
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2022-03-20 22:20:47 +0100

    server: typo

--- src/server/server.js
+++ src/server/server.js
@@ -49,7 +49,7 @@ function resolvePath(url, clientId) {
 
     if (clientId === undefined || !ipc.clients[clientId]) {
         // safari seems to be picky with cookies
-        if (clientId === undefined) console.error(`(W, HTTP) Could not resolve requested url ${url} (client id not found in http cookies)`)
+        if (clientId === undefined) console.error(`(ERROR, HTTP) Could not resolve requested url ${url} (client id not found in http cookies)`)
         // this should never happen, but just in case...
         else console.error(`(ERROR, HTTP) Could not resolve requested url ${url} (unregistered client id ${clientId})`)
     } else {
