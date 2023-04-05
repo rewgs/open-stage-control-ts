645bba0402bea0d2561f2a2dc3a21a4645e7fe3c
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2023-04-06 01:00:23 +0200

    docs: fix opening url with anchors to widget properties

--- resources/docs/extra/scripts.js
+++ resources/docs/extra/scripts.js
@@ -3,9 +3,19 @@ document.addEventListener('DOMContentLoaded', ()=>{
     if (window.location.hash && window.location.href.includes('properties-reference')) {
         // show target in tabs/details
         document.querySelectorAll('[id='+window.location.hash.split("#")[1]+']').forEach(function(item){
-            var tab = item.closest('.tabbed-content'),
-                details = item.closest('details')
-            if (tab) tab.previousSibling.click()
+            var tab = item.closest('.tabbed-block'),
+                container= item.closest('.tabbed-set'),
+                details = item.closest('details'),
+                index = 0
+            while (tab.previousElementSibling) {
+                index++
+                tab = tab.previousElementSibling
+            }
+            console.log(index)
+            container.querySelectorAll('.tabbed-labels label:nth-child('+(index+1)+')').forEach(function(item,i){
+                item.click()
+            })
+
             if (item) {
                 item.scrollIntoView()
                 document.documentElement.scrollTop -= 70
