rewgs/open-stage-control-ts

commit f891442e284d0304651d75022ee970821a108a6e

mobile: apply root's (or themes) background color the the browser's topbar when possible

src/client/ui/utils.js
@@ -1,3 +1,6 @@
+var chroma = require('chroma-js'),
+    fastdom = require('fastdom'),
+    widgetManager = require('../managers/widgets')
 
 module.exports = {
 
@@ -10,6 +13,55 @@ module.exports = {
         return String(string).replace(/\^[^\s]{2,}/g,(x)=>{return module.exports.icon(x.substring(1))})
     },
 
+    setScrollbarColor: function(container) {
 
+        fastdom.measure(()=>{
+
+            var computedStyle = window.getComputedStyle(container),
+                alpha = parseFloat(computedStyle.getPropertyValue('--alpha-scrollbar')),
+                alphaOn = parseFloat(computedStyle.getPropertyValue('--alpha-scrollbar-on')),
+                color = computedStyle.getPropertyValue('--color-fill').trim()
+
+            if (color === 'transparent') {
+                // prevent chroma-js error
+                // irrelevant case, but still...
+                color = 'rgb(0, 0, 0)'
+                alpha = 0
+                alphaOn = 0
+            }
+
+            if (color) {
+                fastdom.mutate(()=>{
+                    try {
+                        container.style.setProperty('--color-scrollbar', chroma(color).alpha(alpha).css())
+                        container.style.setProperty('--color-scrollbar-on', chroma(color).alpha(alphaOn).css())
+                    } catch(e) {}
+                })
+            }
+
+        })
+
+    },
+
+    updateMobileThemeColor: function(){
+
+        fastdom.measure(()=>{
+            var root = widgetManager.getWidgetById('root')[0]
+            var style = window.getComputedStyle(root ? root.widget : document.documentElement)
+
+            fastdom.mutate(()=>{
+
+                var color = style.getPropertyValue('--color-background').trim()
+                try {
+                    for (var el of DOM.get('meta[name="theme-color"], meta[name="apple-mobile-web-app-status-bar-style"]')) {
+                        el.setAttribute('content', color)
+                    }
+                } catch(e) {}
+
+            })
+
+        })
+
+    }
 
 }

src/client/ipc/callbacks.js
@@ -9,7 +9,8 @@ var utils = require('../ui/utils'),
     uiLoading = require('../ui/ui-loading'),
     notifications = require('../ui/notifications'),
     raw = require('nanohtml/raw'),
-    backup = require('../backup')
+    backup = require('../backup'),
+    {updateMobileThemeColor} = require('../ui/utils')
 
 module.exports = {
 
@@ -119,6 +120,7 @@ module.exports = {
                             editor.toggleGrid()
                         })
                     })
+                    updateMobileThemeColor()
                 }
             }
 

src/client/ui/init.js
@@ -5,6 +5,7 @@ require('./notifications')
 require('./ui-workspace')
 require('./main-menu')
 require('./ui-console')
+require('./utils').updateMobileThemeColor()
 
 if (!navigator.userAgent.match(/Android|iPhone|iPad|iPod/i)) {
     require('./ui-keyboard')

src/client/ui/scrollbar-color.js
@@ -1,32 +1,0 @@
-var chroma = require('chroma-js'),
-    fastdom = require('fastdom')
-
-module.exports = (container)=>{
-
-    fastdom.measure(()=>{
-
-        var computedStyle = window.getComputedStyle(container),
-            alpha = parseFloat(computedStyle.getPropertyValue('--alpha-scrollbar')),
-            alphaOn = parseFloat(computedStyle.getPropertyValue('--alpha-scrollbar-on')),
-            color = computedStyle.getPropertyValue('--color-fill').trim()
-
-        if (color === 'transparent') {
-            // prevent chroma-js error
-            // irrelevant case, but still...
-            color = 'rgb(0, 0, 0)'
-            alpha = 0
-            alphaOn = 0
-        }
-
-        if (color) {
-            fastdom.mutate(()=>{
-                try {
-                    container.style.setProperty('--color-scrollbar', chroma(color).alpha(alpha).css())
-                    container.style.setProperty('--color-scrollbar-on', chroma(color).alpha(alphaOn).css())
-                } catch(e) {}
-            })
-        }
-
-    })
-
-}

src/client/ui/ui-workspace.js
@@ -1,5 +1,5 @@
 var UiWidget = require('./ui-widget'),
-    setScrollbarColor = require('./scrollbar-color')
+    {setScrollbarColor} = require('./utils')
 
 class UiWorkspace extends UiWidget {
 

src/client/widgets/containers/panel.js
@@ -3,7 +3,7 @@ var Container = require('../common/container'),
     parser = require('../../parser'),
     html = require('nanohtml'),
     {enableTraversingGestures, disableTraversingGestures} = require('../../events/drag'),
-    setScrollbarColor = require('../../ui/scrollbar-color'),
+    {setScrollbarColor} = require('../../ui/utils'),
     iOS13 = require('../../ui/ios') === 13,
     Fader = require('../sliders/fader'), faderDefaults,
     Script = require('../scripts/script'),

src/client/widgets/containers/root.js
@@ -1,5 +1,6 @@
 var Panel = require('./panel'),
     StaticProperties = require('../mixins/static_properties'),
+    {updateMobileThemeColor} = require('../../ui/utils'),
     mainMenu
 
 class Root extends StaticProperties(Panel, {visible: true, label: false, id: 'root'}) {
@@ -84,6 +85,9 @@ class Root extends StaticProperties(Panel, {visible: true, label: false, id: 'ro
             case 'hideMenu':
                 this.checkMenuVisibility()
                 return
+            case 'colorBg':
+                updateMobileThemeColor()
+
         }
 
     }
@@ -97,6 +101,12 @@ class Root extends StaticProperties(Panel, {visible: true, label: false, id: 'ro
 
     }
 
+    setCssVariables() {
+
+        super.setCssVariables()
+        updateMobileThemeColor()
+
+    }
 
 }
 

src/html/client.html
@@ -6,6 +6,7 @@
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="theme-color" content="black">
     <link rel="apple-touch-icon" href="/assets/logo.png?__OSC_ASSET__=1&version=${version}" />
     <link rel="stylesheet" type="text/css" href="/assets/open-stage-control.css?__OSC_ASSET__=1&version=${version}" hot-reload/>
     <link rel="stylesheet" type="text/css" href="/theme.css?__OSC_ASSET__=1&version=${version}" hot-reload/>

commit 8fb4662bd1b7e64aa31df2f58a68c29f9f268be6

mobile: fix build (broken in f891442e284d0304651d75022ee970821a108a6e)

src/client/ui/utils.js
@@ -1,6 +1,6 @@
 var chroma = require('chroma-js'),
     fastdom = require('fastdom'),
-    widgetManager = require('../managers/widgets')
+    widgetManager
 
 module.exports = {
 
@@ -44,6 +44,8 @@ module.exports = {
     },
 
     updateMobileThemeColor: function(){
+
+        if (!widgetManager) widgetManager = require('../managers/widgets')
 
         fastdom.measure(()=>{
             var root = widgetManager.getWidgetById('root')[0]