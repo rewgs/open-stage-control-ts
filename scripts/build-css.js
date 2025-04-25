48ddb1600b113dbf2be90ee6e17c7c7d56708a13
Alex Ruger <alexrugermusic@gmail.com>
2025-04-25 16:30:31 -0700

    Replaced node-sass with sass.

--- scripts/build-css.js
+++ scripts/build-css.js
@@ -1,44 +1,43 @@
-var sass = require('node-sass'),
-    path = require('path'),
-    fs = require('fs')
+// var sass = require('node-sass'),
+var sass = require("sass"),
+  path = require("path"),
+  fs = require("fs");
 
-var indir = path.resolve(__dirname + '/../src/scss/'),
-    outdir= path.resolve(__dirname + '/../app/assets/')
+var indir = path.resolve(__dirname + "/../src/scss/"),
+  outdir = path.resolve(__dirname + "/../app/assets/");
 
-fs.readdirSync(indir + '/themes/').forEach(file => {
+fs.readdirSync(indir + "/themes/").forEach((file) => {
+  if (file.includes(".scss") && file !== "default.scss") {
+    let result = sass.renderSync({
+      file: indir + "/themes/" + file,
+      outputStyle: "compressed",
+    });
 
-    if (file.includes('.scss') && file !== 'default.scss') {
-        let result = sass.renderSync({
-            file: indir + '/themes/' + file,
-            outputStyle: 'compressed'
-        })
-
-        fs.writeFileSync(outdir + '/themes/' + file.split('/').pop().replace('scss', 'css'), result.css)
-    }
-
-})
+    fs.writeFileSync(
+      outdir + "/themes/" + file.split("/").pop().replace("scss", "css"),
+      result.css
+    );
+  }
+});
 
 var result = sass.renderSync({
-    file: indir + '/index.scss',
-    outFile: outdir + '/open-stage-control.css',
-    outputStyle: 'compressed',
-    sourceMap: true
-})
+  file: indir + "/index.scss",
+  outFile: outdir + "/open-stage-control.css",
+  outputStyle: "compressed",
+  sourceMap: true,
+});
 
-fs.writeFileSync(outdir + '/open-stage-control.css', result.css)
-fs.writeFileSync(outdir + '/open-stage-control.css.map', result.map)
+fs.writeFileSync(outdir + "/open-stage-control.css", result.css);
+fs.writeFileSync(outdir + "/open-stage-control.css.map", result.map);
 
+if (process.argv.includes("--reload")) {
+  var WS = require("../node_modules/ws");
 
-if (process.argv.includes('--reload')) {
+  var ipc = new WS("ws://127.0.0.1:8080/dev/");
 
-    var WS = require('../node_modules/ws')
-
-    var ipc = new WS('ws://127.0.0.1:8080/dev/')
-
-    ipc.on('error', ()=>{})
-    ipc.on('open', ()=>{
-        ipc.send('["reloadCss"]')
-        process.exit()
-    })
-
+  ipc.on("error", () => {});
+  ipc.on("open", () => {
+    ipc.send('["reloadCss"]');
+    process.exit();
+  });
 }
